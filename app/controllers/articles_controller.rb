64f27a460dcf01b5823d984daa81952548105e05
Alex Dulchinos <adulchinos@gmail.com>
2015-04-15 11:32:10 -0500

    cleaned out a few unfinished or unnecessary features

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -74,7 +74,8 @@ class ArticlesController < ApplicationController
 
 	def show
 		@article = Article.find(params[:id])
-		# @comments = Comment.where(article_id: params[:id]) 
+		@newest = Article.order('created_at DESC').limit(12)
+		@most_cited = Article.most_cited
 	end
 
 	
